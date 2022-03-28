cf41de991c1864507195697ac5d43c5e3d0383b3
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2022-03-28 15:17:28 -0300

    add page community

---
+++ src/pages/Community/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CardGames from '../../components/CardGames';
+import Filter from '../../components/Filter';
+import NavBar from '../../components/NavBar';
+import NavDashBoard from '../../components/NavDashBoard';
+import Search from '../../components/Search';
+
+import { Container } from './styles';
+//
+const Community: React.FC = () => {
+  return (
+    <Container>
+      <NavBar />
+      <NavDashBoard setPosition="community" />
+      <div id="line-arctions">
+        <Search />
+        <Filter />
+      </div>
+      <div>
+        <CardGames />
+        <CardGames />
+        <CardGames />
+        <CardGames />
+      </div>
+    </Container>
+  );
+};
+
+export default Community;
---
+++ src/pages/Community/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  #line-arctions {
+    display: flex;
+    flex-direction: row;
+
+    align-items: center;
+    margin: 55px auto auto 150px;
+  }
+`;
