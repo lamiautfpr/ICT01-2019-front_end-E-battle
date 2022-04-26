2120a97a3fb931fc93c91ab4beb9b4e165dd2bde
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2022-04-26 09:13:20 -0300

    test sem imagem

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IoPersonCircleOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/logo.png';
+// import Logo from '../../assets/logo.png';
 
 import { Container } from './styles';
 
@@ -9,9 +9,7 @@ const NavBar: React.FC = () => {
   return (
     <Container>
       <div>
-        <Link to="/">
-          <img src={Logo} alt="" />
-        </Link>
+        <Link to="/">{/* <img src={Logo} alt="" /> */}</Link>
         <ul>
           <li>
             <Link to="/login">Jogar</Link>
