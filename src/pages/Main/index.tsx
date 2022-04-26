f6016d337368e1e7e798eb48b2306a130655b6ec
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2022-04-26 09:13:47 -0300

    as

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../../components/NavBar';
-import Blob from '../../assets/blob.png';
+// import Blob from '../../assets/blob.png';
 import Button from '../../components/Button';
 
 import { Container, Central } from './styles';
@@ -23,7 +23,7 @@ const Main: React.FC = () => {
           </Button>
           {/* <button type="button">Jogar</button> */}
         </div>
-        <img src={Blob} alt="blod" />
+        {/* <img src={Blob} alt="blod" /> */}
       </Central>
     </Container>
   );
