d3c4ab080f18b82d75c166eea964c15f8a264bb3
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2022-05-02 09:37:42 -0300

    add hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/globals';
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <AppProvider>
+      <Router>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </AppProvider>
   );
 };
 
---
+++ src/hooks/Auth.tsx
@@ -0,0 +1,120 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import api from '../services/api';
+// import { ImageProps } from '../../myTypes/Images';
+// import { SelectItem } from '../../myTypes/SelectItem';
+// import { useToast } from './Toast';
+
+export interface IUserProps {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  status: number;
+  institution: string;
+  city: string;
+  workType: string;
+  educationLevel: string;
+}
+
+interface IAuthState {
+  access_token: string;
+  // user: IUserProps;
+}
+
+interface ISignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface IAuthProviderData {
+  access_token: string;
+  // user: IUserProps;
+  signIn(credentials: ISignInCredentials): Promise<void>;
+  signOut(): void;
+  updateMember(member: IUserProps): void;
+}
+
+// export const officesPermitted: number[] = [1, 2, 3];
+const AuthContext = createContext<IAuthProviderData>({} as IAuthProviderData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  // const { addToast } = useToast();
+
+  const [data, setData] = useState<IAuthState>(() => {
+    const access_token = localStorage.getItem('@LAMIA:token');
+    // const user = localStorage.getItem('@LAMIA:user');
+
+    // if (access_token && user) {
+    //   return { access_token, user: JSON.parse(user) };
+    // }
+    if (access_token) {
+      return { access_token };
+    }
+
+    return {} as IAuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    try {
+      const response = await api.post('auth/login', {
+        email,
+        password,
+      });
+
+      const { access_token, member } = response.data;
+
+      localStorage.setItem('@LAMIA:token', access_token);
+      // localStorage.setItem('@LAMIA:member', JSON.stringify(member));
+
+      // setData({ access_token, member });
+      setData({ access_token });
+    } catch (error) {
+      // addToast({
+      //   type: 'error',
+      //   title: 'Erro na autenticação',
+      //   description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
+      // });
+    }
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@LAMIA:token');
+    // localStorage.removeItem('@LAMIA:member');
+  }, []);
+
+  const updateMember = useCallback(
+    (user: IUserProps) => {
+      setData({
+        access_token: data.access_token,
+        // member,
+      });
+      // localStorage.setItem('@LAMIA:member', JSON.stringify(member));
+    },
+    [data.access_token],
+  );
+
+  return (
+    <AuthContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        access_token: data.access_token,
+        // member: data.member,
+        signIn,
+        signOut,
+        updateMember,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): IAuthProviderData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('Usou Auth sem a inicialização do AuthProvider');
+  }
+
+  return context;
+}
